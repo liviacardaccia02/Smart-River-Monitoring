3be1b732d9c55bbfed0847f7787d2e54209cd1ab
nonsonotino <matteo.sartini26@gmail.com>
2024-03-06 10:53:29 +0100

    ARDUINO: added % getter to Potentiometer class

--- water-channel-controller/lib/devices/Potentiometer.cpp
+++ water-channel-controller/lib/devices/Potentiometer.cpp
@@ -11,3 +11,8 @@ int Potentiometer::getValue()
 {
     return analogRead(pin);
 }
+
+int Potentiometer::getPercent()
+{
+    return map(this->getValue(),MIN_REAL_VALUE, MAX_REAL_VALUE, MIN_PERCENT, MAX_PERCENT);
+}
--- water-channel-controller/lib/devices/Potentiometer.h
+++ water-channel-controller/lib/devices/Potentiometer.h
@@ -1,6 +1,11 @@
 #ifndef __POTENTIOMETER__
 #define __POTENTIOMETER__
 
+#define MIN_REAL_VALUE 0
+#define MAX_REAL_VALUE 1023
+#define MIN_PERCENT 0
+#define MAX_PERCENT 100
+
 class Potentiometer
 {
 private:
@@ -9,6 +14,7 @@ private:
 public:
     Potentiometer(int pin);
     int getValue();
+    int getPercent();
 };
 
 #endif
