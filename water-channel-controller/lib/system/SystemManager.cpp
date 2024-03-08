797f15a6f4f3bf475c1fcf4fff7aa5bfa1e37c73
nonsonotino <matteo.sartini26@gmail.com>
2024-03-08 17:10:44 +0100

    ARDUINO: added first iteration of system manager class

---
+++ water-channel-controller/lib/system/SystemManager.cpp
@@ -0,0 +1,73 @@
+#include "SystemManager.h"
+
+SystemManager::SystemManager(int buttonPin, int potPin, int servoPin)
+{
+    button = new Button(buttonPin);
+    pot = new Potentiometer(potPin);
+    servo = new ServoMotor(servoPin);
+    lcdDisplay = new LCD();
+    prevLevel = 0;
+    mode = MANUAL; //Modify to change the starting mode
+}
+
+void SystemManager::updateBoard(int gateLevel)
+{
+    displayMode();
+    lcdDisplay->clearLine(2);
+    lcdDisplay->WriteOnLine("Gate level: " +  (String)gateLevel +"%" , 2);
+    servo->openPercent(gateLevel);
+}
+
+void SystemManager::displayMode()
+{
+    lcdDisplay->clearLine(1);
+    if(mode == AUTOMATIC)
+    {
+        lcdDisplay->WriteOnLine("Mode: AUTOMATIC" , 1);
+    }
+    else if(mode == MANUAL)
+    {
+        lcdDisplay->WriteOnLine("Mode: MANUAL" , 1);
+    }
+}
+
+void SystemManager::updateManual()
+{
+    //Get gate opening info from potentiometer
+    int gateLevel = pot->getPercent();
+
+    //Display gate level on diplay
+    //TODO display mode? where
+    if(gateLevel != prevLevel){
+        updateBoard(gateLevel);
+    }
+
+    prevLevel = gateLevel;
+
+    //Update servo
+
+    //Communicate with application
+    //TODO
+}
+
+void SystemManager::updateAutomatic()
+{
+    //Communication with application
+}
+
+int SystemManager::checkMode()
+{
+    if(button->isPressed())
+    {
+        if(mode == AUTOMATIC)
+        {
+            mode = MANUAL;
+        }
+        else
+        {
+            mode = AUTOMATIC;
+        }
+    }
+
+    return mode;
+}
