3759eef18c4e144365fe3e463fd5f61de288a1d9
liviacardaccia02 <liviacardaccia99@gmail>
2024-03-13 16:18:15 +0100

    ESP32: green and red leds working as they should

--- water-level-monitoring-subsystem/src/main.cpp
+++ water-level-monitoring-subsystem/src/main.cpp
@@ -2,28 +2,35 @@
 #include <Wire.h>
 #include <PubSubClient.h>
 #include <WiFi.h>
+#include <Sonar.h>
 
 #define MSG_BUFFER_SIZE (500)
 #define WIFI_TIMEOUT 20000
 #define WIFI_SSID "Livia’s iPhone" // Replace with your own SSID
 #define WIFI_PASSWORD "kitty123"   // Replace with your own password
 
+#define RED_LED_PIN 1
+#define GREEN_LED_PIN 2
+#define TRIG_PIN 13
+#define ECHO_PIN 14
+
 WiFiClient espClient;
 PubSubClient client(espClient);
+Sonar* sonar;
 
 const char *mqtt_broker = "broker.hivemq.com";
-const char *topic = "testTopic";
+const char *topic = "WaterLevel";
 const char *mqtt_username = "liviacardaccia";
 const char *mqtt_password = "public";
 const int mqtt_port = 1883;
 
-long lastMsg = 0;
+unsigned long lastPublishTime = 0;
 char msg[MSG_BUFFER_SIZE];
 int value = 0;
 
 void connectToWifi()
 {
-  delay(100);
+  delay(200);
   Serial.println("Connecting to WiFi...");
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -38,16 +45,20 @@ void connectToWifi()
 
   if (WiFi.status() != WL_CONNECTED)
   {
+    digitalWrite(RED_LED_PIN, HIGH);
+    digitalWrite(GREEN_LED_PIN, LOW); // disconnesso
     Serial.println("Failed to connect to WiFi.");
     Serial.println("Restarting in 5 seconds...");
     delay(5000);
-    ESP.restart();
+    ESP.restart(); // TODO: add a counter to restart only after a certain number of attempts
   }
   else
   {
+    digitalWrite(RED_LED_PIN, LOW);
+    digitalWrite(GREEN_LED_PIN, HIGH); // connesso
     Serial.println("Connected to WiFi.");
     Serial.println("IP address: ");
-    Serial.print(WiFi.localIP());
+    Serial.println(WiFi.localIP());
   }
 }
 
@@ -55,30 +66,36 @@ void reconnect()
 {
   while (!client.connected())
   {
-    Serial.println("Connecting to MQTT...");
+    Serial.println("Connecting to MQTT...\n");
 
     String clientID = "ESP32Client-";
     clientID += String(random(0xffff), HEX);
 
     if (client.connect(clientID.c_str(), mqtt_username, mqtt_password))
     {
+      digitalWrite(RED_LED_PIN, LOW);
+      digitalWrite(GREEN_LED_PIN, HIGH); // mqtt okay
       Serial.println("Connected to MQTT");
     }
     else
     {
+      digitalWrite(RED_LED_PIN, HIGH);
+      digitalWrite(GREEN_LED_PIN, LOW); // mqtt not okay
       Serial.println("Failed, code = ");
-      Serial.print(client.state());
+      Serial.println(client.state());
       Serial.println("Try again in 5 seconds");
       delay(5000);
     }
   }
 }
 
-void callback(char* topic, byte* payload, unsigned int length) {
+void callback(char *topic, byte *payload, unsigned int length)
+{
   Serial.print("Messaggio ricevuto su topic: ");
   Serial.println(topic);
   Serial.print("Contenuto: ");
-  for (int i = 0; i < length; i++) {
+  for (int i = 0; i < length; i++)
+  {
     Serial.print((char)payload[i]);
   }
   Serial.println();
@@ -87,29 +104,42 @@ void callback(char* topic, byte* payload, unsigned int length) {
 void setup()
 {
   Serial.begin(115200);
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(RED_LED_PIN, OUTPUT);
+  pinMode(GREEN_LED_PIN, OUTPUT);
   connectToWifi();
-  client.setServer(mqtt_broker, mqtt_port);  // Port 1883 for MQTT
+  client.setServer(mqtt_broker, mqtt_port); 
   client.setCallback(callback);
-  client.publish(topic, "hello world");
-  client.subscribe(topic);
 }
 
 void loop()
 {
+  if (WiFi.status() != WL_CONNECTED)
+  {
+    digitalWrite(RED_LED_PIN, HIGH);
+    digitalWrite(GREEN_LED_PIN, LOW);
+    Serial.println("WiFi connection lost. Reconnecting...");
+    connectToWifi();
+  }
+
   if (!client.connected())
   {
+    digitalWrite(RED_LED_PIN, HIGH);
+    digitalWrite(GREEN_LED_PIN, LOW);
+    Serial.println("MQTT connection lost. Reconnecting...");
     reconnect();
   }
-  client.loop();
 
-  unsigned long now = millis();
-  if (now - lastMsg > 2000) {
-    lastMsg = now;
-    ++value;
-    snprintf (msg, MSG_BUFFER_SIZE, "hello world #%ld", value);
-    Serial.print("Publish message: ");
-    Serial.println(msg);
-    client.publish("testTopic", msg);
+  unsigned long currentTime = millis();
+
+  if (currentTime - lastPublishTime >= 1000) {  
+    // Publishing every 1000 milliseconds (1 second)
+    // TODO: add code to read the distance from the sensor
+
+    Serial.println("Publishing distance...");
+    
+    lastPublishTime = currentTime;
   }
+
+  client.loop();
+  delay(1000);
 }
