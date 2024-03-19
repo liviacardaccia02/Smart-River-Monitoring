45c750227f5937ab626112feb7218b17078397a5
nonsonotino <matteo.sartini26@gmail.com>
2024-03-19 09:35:04 +0100

    ARDUINO: added function to modify current mode trought Serial line

--- water-channel-controller/lib/communication/MessageHandler.cpp
+++ water-channel-controller/lib/communication/MessageHandler.cpp
@@ -1,18 +1,41 @@
 #include "MessageHandler.h"
 
+String MessageHandler::getMessage()
+{
+    String msg = "";
+    if(Serial.available())
+    {
+        msg = Serial.readStringUntil('\n');
+    }
+    return msg;
+}
+
 int MessageHandler::getValue()
 {
     if(Serial.available())
     {
-        String msg = Serial.readStringUntil('\n');
-        Serial.println("Recieved " + msg);
-        if(msg.indexOf("VAL") >= 0)
+        String msg = this->getMessage();
+        if(msg.indexOf(VALUE_PREFIX) >= 0)
         {
             msg.remove(0,3);
-            Serial.println("Modified " + msg);
+            Serial.println("Received: " + msg);
             return msg.toInt();
         }
     }
 
     return -1; //Returns negative value if there is no message incoming
+}
+
+int MessageHandler::getType()
+{
+    //TODO
+    String msg = this->getMessage();
+    if(msg.indexOf(MODE_PREFIX) >= 0)
+    {
+        msg.remove(0,3);
+        Serial.println("Received mode: " + msg);
+        return msg.toInt();
+    }
+    
+    return -1;
 }
--- water-channel-controller/lib/communication/MessageHandler.h
+++ water-channel-controller/lib/communication/MessageHandler.h
@@ -3,12 +3,21 @@
 
 #include <Arduino.h>
 
+#define VALUE_PREFIX "VAL"
+#define MODE_PREFIX "MOD"
+#define MSG_VAL 0
+#define MSG_MOD 1
+#define MODE_AUTOMATIC 0
+#define MODE_MANUAL 1
+
 class MessageHandler
 {
 private:
     
 public:
+    String getMessage();
     int getValue();
+    int getType();
 };
 
 #endif
