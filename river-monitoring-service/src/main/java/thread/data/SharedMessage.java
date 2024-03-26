c90c4d4e1f77a15434905af9bf644fb7c52518ed
giovanniprete <g.prete@cosmobile.com>
2024-03-26 13:16:17 +0100

    added .gitignore

---
+++ river-monitoring-service/src/main/java/thread/data/SharedMessage.java
@@ -0,0 +1,14 @@
+package thread.data;
+
+public class MessaggioCondiviso {
+    private String messaggio;
+
+    public synchronized String getMessaggio() {
+        return messaggio;
+    }
+
+    public synchronized void setMessaggio(String messaggio) {
+        this.messaggio = messaggio;
+        notifyAll(); // Notifica tutti i thread in attesa sul messaggio
+    }
+}
